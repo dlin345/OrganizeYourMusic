80da0316ed9ff96691beee1004dc3bd640d56e62
Unknown <dlin345@gmail.com>
2018-02-07 16:23:08 -0800

    Added 3d plot tab, test plot tab

--- README.md
+++ README.md
@@ -3,6 +3,16 @@
 
 A web app that lets you organize your music.
 
-Online at http://static.echonest.com/OrganizeYourMusic
+View the original web application online at http://static.echonest.com/OrganizeYourMusic
 
 
+## Usage
+
+Run the site locally using a python server:
+
+  cd web
+  python -m SimpleHTTPServer 8000
+
+Then visit the site at:
+
+  http://localhost:8000/
--- web/config.js
+++ web/config.js
@@ -1,6 +1,18 @@
 "use strict";
 
-var SPOTIFY_CLIENT_ID = '1c81d0d03de148c083744f5cce782ef7';
+/**
+  Run the site locally e.g. using a python server:
+    cd web
+    python -m SimpleHTTPServer 8000
+
+  Then visit the site at:
+    http://localhost:8000/
+
+*/
+
+// var SPOTIFY_CLIENT_ID = 'f43651fc7357468bbbd59f26dc5f4f48'; // my clientID for apache2 server
+// var LOCAL_SPOTIFY_REDIRECT_URI = 'http://localhost/'; // redirect URI for apache2 server
+var SPOTIFY_CLIENT_ID = '1c81d0d03de148c083744f5cce782ef7'; // for SimpleHTTPServer
+var LOCAL_SPOTIFY_REDIRECT_URI = 'http://localhost:8000/'; // for SimpleHTTPServer
 var REMOTE_SPOTIFY_REDIRECT_URI = 'http://static.echonest.com/OrganizeYourMusic/'
-var LOCAL_SPOTIFY_REDIRECT_URI = 'http://localhost:8000/';
 var SPOTIFY_REDIRECT_URI = REMOTE_SPOTIFY_REDIRECT_URI;
